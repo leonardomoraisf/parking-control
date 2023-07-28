6970d660738a78f540cff9b8457057ad2609a28f
Leonardo Morais <mfrancaleonardo@gmail.com>
2023-07-28 19:39:17 -0300

    feat: método para salvar

--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -2,7 +2,10 @@ package com.api.parkingcontrol.services;
 
 import org.springframework.stereotype.Service;
 
+import com.api.parkingcontrol.models.ParkingSpotModel;
 import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+
+import jakarta.transaction.Transactional;
 
 @Service
 public class ParkingSpotService {
@@ -11,5 +14,10 @@ public class ParkingSpotService {
 
     public ParkingSpotService(ParkingSpotRepository parkingSpotRepository) {
         this.parkingSpotRepository = parkingSpotRepository;
+    }
+
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel parkingSpotModel) {
+        return parkingSpotRepository.save(parkingSpotModel);
     }
 }
